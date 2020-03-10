5f1d041eeb2e3c049f825f67cf6c68e5540fdcff
Suengyong <catastro728@gmail.com>
2020-03-10 11:13:37 +0900

    Updates

---
+++ precache-manifest.b6080dbb0ffa961defba89823a0aa204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de111ee5b1907327978e652601d63b9",
+    "url": "/2020-high-rating-movie/index.html"
+  },
+  {
+    "revision": "5233d1153a416dd08c2d",
+    "url": "/2020-high-rating-movie/static/css/main.60d2e14c.chunk.css"
+  },
+  {
+    "revision": "aa768e084ec5ab1bea00",
+    "url": "/2020-high-rating-movie/static/js/2.48fc7ec5.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/2020-high-rating-movie/static/js/2.48fc7ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5233d1153a416dd08c2d",
+    "url": "/2020-high-rating-movie/static/js/main.58f30937.chunk.js"
+  },
+  {
+    "revision": "d8d585a3e843f20bac8c",
+    "url": "/2020-high-rating-movie/static/js/runtime-main.046371ea.js"
+  }
+]);
